3da197fcf18b7d2a16cdac6d2cd3cee6c5f13aec
蔡晓梅 <2513018185@qq.com>
2020-05-25 21:03:39 +0800

    引入iconfont字体图标

--- src/layout/components/Aside.vue
+++ src/layout/components/Aside.vue
@@ -7,31 +7,31 @@
     active-text-color="#ffd04b"
   >
     <el-menu-item index="/home">
-      <i class="el-icon-menu"></i>
+      <i class="iconfont icon-yingyongjiankong"></i>
       <span slot="title">应用监控</span>
     </el-menu-item>
     <el-menu-item index="/laboratory/index">
-      <i class="el-icon-setting"></i>
+      <i class="iconfont icon-ico_shiyanshiguanli_shebeiweihuchaxun"></i>
       <span slot="title">实验室管理</span>
     </el-menu-item>
     <el-menu-item index="/recruit/index">
-      <i class="el-icon-setting"></i>
+      <i class="iconfont icon-zhaopin"></i>
       <span slot="title">招聘管理</span>
     </el-menu-item>
     <el-menu-item index="/enroll/index">
-      <i class="el-icon-setting"></i>
+      <i class="iconfont icon-baomingguanli"></i>
       <span slot="title">报名管理</span>
     </el-menu-item>
     <el-menu-item index="/interview/index">
-      <i class="el-icon-setting"></i>
+      <i class="iconfont icon-mianshiguanli"></i>
       <span slot="title">面试管理</span>
     </el-menu-item>
     <el-menu-item index="/work/index">
-      <i class="el-icon-setting"></i>
+      <i class="iconfont icon-gongzuoguanli"></i>
       <span slot="title">工作管理</span>
     </el-menu-item>
     <el-menu-item index="/repair/index">
-      <i class="el-icon-setting"></i>
+      <i class="iconfont icon-weixiuguanli"></i>
       <span slot="title">维修管理</span>
     </el-menu-item>
   </el-menu>
